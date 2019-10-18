f3564a1df34a7b2b24739077d5c25cfd8bf80d6a
Douglas Hoffmann <douglas@oursound.com.br>
2019-10-18 13:45:14 -0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-southeast-1",
+      BUCKET: "curso-serverless-notes"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-1",
+      URL: "https://bj94r3ffa8.execute-api.ap-southeast-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-southeast-1",
+      USER_POOL_ID: "ap-southeast-1_rXLILRYgF",
+      APP_CLIENT_ID: "58k5no2908shihe5o44d9tnsp8",
+      IDENTITY_POOL_ID: "ap-southeast-1:c8a6e0e1-4cdd-42de-b89e-7b6548c42be4"
+    }
+  };
